4f5199245144b821bf33e063d942492ed093fc9b
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-07-03 09:25:42 -0300

    feat(upload video)

--- src/modules/videos/repositories/videoRepository.js
+++ src/modules/videos/repositories/videoRepository.js
@@ -6,11 +6,11 @@ class VideoRepository {
 
 
   async uploadVideo (req, res) {
-    const video = req.file
+    const video = req.file 
     console.log(video)
     const API_KEY = 'panda-c57b79e21cde1ae62730d8067fef00e6953bbb1afbbb9868519561e8b1b5d68d'
     const FOLDER_ID = null
-    const FILENAME = 'VIDEO TESTE'
+    const FILENAME = req.file.originalname
     const VIDEO_ID = uuidv4()
     const parseToBase64 = string=>Buffer.from(string).toString('base64')
     const binaryFile = fs.readFileSync(req.file.path);
