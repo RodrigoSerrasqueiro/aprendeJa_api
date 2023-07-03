36ae1a9f0595421b32b97b37336ddaaeb25387b5
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-07-03 08:50:06 -0300

    feat(upload video)

--- src/modules/videos/repositories/videoRepository.js
+++ src/modules/videos/repositories/videoRepository.js
@@ -1,17 +1,62 @@
-import fs from 'fs';
+import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
+import fs from 'fs'
 
 class VideoRepository {
-  async uploadVideo (req, res) {
-    const { video } = req.file;
-    try {
-      // Fazer o processamento necessário com o arquivo de vídeo
-      // Exemplo: mover o arquivo para um diretório permanente
-      const newPath = `uploads/${video.originalname}`;
-      fs.renameSync(video.path, newPath);
 
-      res.status(200).json({ message: "Chegou algo aqui", video: newPath });
+
+  async uploadVideo (req, res) {
+    const video = req.file
+    console.log(video)
+    const API_KEY = 'panda-c57b79e21cde1ae62730d8067fef00e6953bbb1afbbb9868519561e8b1b5d68d'
+    const FOLDER_ID = null
+    const FILENAME = 'VIDEO TESTE'
+    const VIDEO_ID = uuidv4()
+    const parseToBase64 = string=>Buffer.from(string).toString('base64')
+    const binaryFile = fs.readFileSync(req.file.path);
+
+    let metadata = `authorization ${parseToBase64(API_KEY)}`
+    if(FOLDER_ID){ 
+      metadata += `, folder_id ${parseToBase64(FOLDER_ID)}`
+    }
+    metadata += `, filename ${parseToBase64(FILENAME)}`
+    metadata += `, video_id ${parseToBase64(VIDEO_ID)}`
+  
+    try {
+      const {data: uploadServers} = await axios.get('https://api-v2.pandavideo.com.br/hosts/uploader',{
+        headers:{
+          'Authorization': API_KEY,
+        }
+      });
+      const allHosts = Object.values(uploadServers.hosts).reduce((acc,curr)=>([...acc,...curr]),[]);
+      const host = allHosts[Math.floor(Math.random() * allHosts.length)];
+      console.log(`Starting upload to ${host}`);
+  
+      // Step 1
+      // Create a video URL to upload content in the second step
+      const { headers } = await axios.post(`https://${host}.pandavideo.com.br/files`, false, {
+        headers:{
+          'Tus-Resumable': '1.0.0', 
+          'Upload-Length': binaryFile.byteLength,
+          'Upload-Metadata': metadata
+        }
+      });
+  
+      // Step 2
+      // Send the video content in the URL received in the header of the first step
+      // Create a video URL to upload content in the second step
+      await axios.patch(`${headers.location}`, Buffer.from(binaryFile, 'binary'), {
+        headers:{
+          'Upload-Offset': 0,
+          'Tus-Resumable': '1.0.0',
+          'Content-Type': 'application/offset+octet-stream'
+        }
+      });
+  
+      console.log('Upload completed successfully');
     } catch (error) {
-      res.status(500).json({ message: "Erro no servidor" });
+      console.log('UPLOAD ERROR');
+      console.log({message: "Deu erro", error: error});
     }
   }
 }
